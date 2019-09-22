889378fde76a1c1ad06ba75d8e03b502a079b556
Patrick Kuijpers <patrickuijpers@gmail.com>
2019-09-22 22:44:51 +0200

    Add ratelimiter to prevent calling the API constantly

--- app/src/main/java/nl/tcilegnar/mygithub/data/UserRepository.kt
+++ app/src/main/java/nl/tcilegnar/mygithub/data/UserRepository.kt
@@ -6,24 +6,31 @@ import nl.tcilegnar.mygithub.api.Resource
 import nl.tcilegnar.mygithub.db.UserDao
 import nl.tcilegnar.mygithub.model.User
 import nl.tcilegnar.mygithub.AppExecutors
+import nl.tcilegnar.mygithub.util.RateLimiter
 
 class UserRepository(
     private val executors: AppExecutors,
     private val userDao: UserDao,
     private val githubService: GitHubService
 ) {
+    private val repoListRateLimit = RateLimiter<String>(1, TimeUnit.DAYS)
+
     fun loadUser(userName: String): LiveData<Resource<User>> {
         return object : NetworkBoundResource<User, User>(executors) {
             override fun saveCallResult(item: User) {
                 userDao.insert(item)
             }
 
-            override fun shouldFetch(data: User?) = data == null // TODO (PK): once a day / week?
-            // TODO (PK): Does avatar link change when new one is uploaded?
+            override fun shouldFetch(data: User?): Boolean {
+                // TODO (PK): Does avatar link change when new one is uploaded?
+                return data == null || repoListRateLimit.shouldFetch(userName)
+            }
 
             override fun loadFromDb() = userDao.findByLogin(userName)
 
             override fun createCall() = githubService.getUser(userName)
+
+            override fun onFetchFailed() = repoListRateLimit.reset(userName)
         }.asLiveData()
     }
 }
